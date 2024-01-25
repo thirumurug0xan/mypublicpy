91788889a42e7b6981dfe79f863b7c6378e2180f
thirumurug0xan <thirumurugann28@gmail.com>
2024-01-25 16:52:06 +0000

    modified:   prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -7,7 +7,7 @@ if not rang:
 
 for i in range(0,rang+1):
     if i > 1:
-        for j in range(2,int(i/2)+1):
+        for j in range(2,i):
             if (i % j) == 0:
                 break
         else:
